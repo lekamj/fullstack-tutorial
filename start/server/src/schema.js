2b2a099830a193210d2121156d5b402713beb60e
lekamj <hannar.j.lee@gmail.com>
2019-05-13 15:07:07 -0400

    playing around with apollo

---
+++ start/server/src/schema.js
@@ -0,0 +1,62 @@
+const { gql } = require('apollo-server');
+const typeDefs = gql`
+  type Query {
+    launches(
+      pageSize: Int
+      after: String
+    ) : LaunchConnection!
+    launch(id: ID!):Launch
+    me: User
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPAtch(size: PatchSize):String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    cancelTrips(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]
+  }
+`;
+module.exports = typeDefs;
